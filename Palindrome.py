a154311fd7d3dad5f7927461bc999002da776174
SATYAMKAHALI <satyamkahali66@gmail.com>
2023-11-14 21:52:40 +0530

    Palindrome

---
+++ Palindrome.py
@@ -0,0 +1,17 @@
+class Palindrome:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        a_pointer = 0
+        b_pointer = 0
+        max_length = 0
+        hash_set = set()
+        
+        while b_pointer < len(s):
+            if s[b_pointer] not in hash_set:
+                hash_set.add(s[b_pointer])
+                b_pointer += 1
+                max_length = max(len(hash_set), max_length)
+            else:
+                hash_set.remove(s[a_pointer])
+                a_pointer += 1
+                
+        return max_length
